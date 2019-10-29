41034d65939957c14ce36d90e5be832f65a64d3d
Joaquim Codina <joaquim.codina@e-campus.uab.cat>
2019-10-29 22:44:50 +0100

    Create method in Min Class

--- src/Min.java
+++ src/Min.java
@@ -1,3 +1,25 @@
-public class Min {
+import java.util.Arrays;
 
+public class Min {
+    public static int[] init(Integer setA[], Integer setB[], int toChase){
+
+        int min[] = new int[2];
+
+        //Sort ascending lists
+        Arrays.sort(setA);
+        Arrays.sort(setB);
+
+        for (int i = 0; i < setA.length; i++){
+            if(setA[i] <= toChase){
+                min[0] = setA[i];
+            }
+        }
+
+        for (int i = 0; i < setB.length; i++){
+            if(setB[i] <= toChase){
+                min[1] = setB[i];
+            }
+        }
+        return min;
+    }
 }
