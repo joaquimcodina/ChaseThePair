dc39afaf50556286e1e36fe958edc7d05e99ae62
Joaquim Codina <joaquim.codina@e-campus.uab.cat>
2019-10-29 22:40:46 +0100

    Create Main Class and initialize toChase and arrays

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+
+        //Initialize toChase and arrays
+        int toChase = 38;
+        Integer setA[] = {81,87,47,59,81,18,25,40,56,0};
+        Integer setB[] = {94,11,62,89,28,74,11,45,37,6};
+    }
+}
