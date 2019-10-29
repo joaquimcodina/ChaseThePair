d3ee31cbb1931df274ed72a52071795182fbee36
Joaquim Codina <joaquim.codina@e-campus.uab.cat>
2019-10-29 22:42:40 +0100

    Call methods

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
 
@@ -5,5 +7,10 @@ public class Main {
         int toChase = 38;
         Integer setA[] = {81,87,47,59,81,18,25,40,56,0};
         Integer setB[] = {94,11,62,89,28,74,11,45,37,6};
+
+        int min[] = Min.init(setA, setB, toChase);
+        int max[] = Max.init(setA, setB, toChase);
+        int result[] = Result.init(min, max, toChase);
+
     }
 }
