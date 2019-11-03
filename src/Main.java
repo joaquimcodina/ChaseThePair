597ff1a338c8a7ff6a1f56cbeb66a79ae61e71d7
Joaquim Codina <joaquim.codina@e-campus.uab.cat>
2019-11-03 22:35:38 +0100

    Call methods GetSets class in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,34 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         //Initialize toChase and arrays
-        int toChase = 45;
-        Integer setA[] = {81,87,47,59,81,18,25,40,56,0};
-        Integer setB[] = {94,11,62,89,28,74,11,45,37,6};
+        int toChase = 231;
+        Integer setA[] = {1,23,45,66,84,113,145,178,205,210,221,250,264,300};
+        Integer setB[] = {5,31,40,52,73,103,126,162,195,214,225,241,256,267};
+
+        //Read file and get sets
+        File logs = new File("logs.txt");
+        GetSets.runExe(logs);
+        setA = GetSets.getSetA(setA, logs);
+        setB = GetSets.getSetB(setB, logs);
+        toChase = GetSets.getRandomNumber(logs, toChase);
+        logs.delete();
 
         //Execute operations
         long start = System.currentTimeMillis();
-        int min[] = Min.init(setA, setB, toChase);
         int max[] = Max.init(setA, setB, toChase);
+        int min[] = Min.init(setA, setB, toChase);
         int result[] = Result.init(min, max, toChase);
         long end = System.currentTimeMillis();
 
         //Print result
         System.out.println("Number to chase: " + toChase+"\n" +
+                "SetA: " + Arrays.toString(setA)+"\n" +
+                "SetB: " + Arrays.toString(setB)+"\n" +
                 "Result: " + Arrays.toString(result)+"\n" +
                 "Time: "+(end - start)/1000.0+"s");
     }
